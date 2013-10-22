ad70c4771a286a806324c5e26fef87c4a27bfbb1
Spencer Fornaciari <mail@spencerfornaciari.com>
2013-10-21 20:41:34 -0700

    Initial Commit

---
+++ PostApp/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  PostApp
+//
+//  Created by Spencer Fornaciari on 10/21/13.
+//  Copyright (c) 2013 Spencer Fornaciari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PostApp/SFViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFViewController.h
+//  PostApp
+//
+//  Created by Spencer Fornaciari on 10/21/13.
+//  Copyright (c) 2013 Spencer Fornaciari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFViewController : UIViewController
+
+@end
